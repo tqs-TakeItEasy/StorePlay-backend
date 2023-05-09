9976ea9b5fe73a7ffdce2917f64c84a6d7f68637
Tiago Sora <tiagogcarvalho2002@gmail.com>
2023-05-09 23:01:09 +0100

    spring boot set up

---
+++ src/main/java/storeplay/backend/StoreplayBackendApplication.java
@@ -0,0 +1,13 @@
+package storeplay.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreplayBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreplayBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/storeplay/backend/StoreplayBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package storeplay.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreplayBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
